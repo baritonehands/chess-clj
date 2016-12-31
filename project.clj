c64c9799f9a007d8a53136c17fa9d0b038b63357
Brian Gregg <bgregg@grubhub.com>
2016-12-31 15:38:02 -0600

    Improved printing.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.aviso/pretty "0.1.33"]]
   :main ^:skip-aot chess.core
   :target-path "target/%s"
   :repl-options {:init-ns user}
--- src/chess/print.clj
+++ src/chess/print.clj
@@ -1,41 +1,49 @@
 (ns chess.print
   (:require [clojure.string :as s]
             [chess.rules :as rules]
-            [chess.board :refer :all]))
+            [chess.board :refer :all]
+            [io.aviso.ansi :refer :all]))
 
 (defn display-name [piece]
-  (if-not piece
-    ".."
-    (str (s/upper-case (first (name (:color piece))))
-         ({:pawn   "P"
-           :rook   "R"
-           :knight "N"
-           :queen  "Q"
-           :bishop "B"
-           :king   "K"} (:rank piece)))))
+  (let [color (:color piece)
+        offset (if (= color :black) 6 0)]
+    (if-not piece
+      "   "
+      (str (if (= (:color piece) :black)
+             (str bold-white-font black-bg-font)
+             (str bold-black-font white-bg-font))
+           " "
+           ({:pawn   "P"
+             :rook   "R"
+             :knight "N"
+             :queen  "Q"
+             :bishop "B"
+             :king   "K"} (:rank piece))
+           " "
+           reset-font))))
 
 (defn board [pm]
-  (println (str "  |" (s/join "|" (map #(str "  " % " ") (range 0 8))) "|"))
-  (println (str "--+" (s/join "+" (repeat 8 "----")) "+"))
+  (println (str "  |" (s/join "|" (map #(str "  " % "  ") (range 0 8))) "|"))
+  (println (str "--+" (s/join "+" (repeat 8 "-----")) "+"))
   (doseq [row (range 0 8)]
     (print row "|")
     (doseq [col (range 0 8)]
       (print (str " " (display-name (piece-at pm [col row])) " |")))
-    (print "\n"))
-  (println (str "--+" (s/join "+" (repeat 8 "----")) "+")))
+    (println (str "\n--+" (s/join "+" (repeat 8 "-----")) "+"))))
 
-(defn moves [moves pos]
-  (println (str "+" (s/join "+" (repeat 8 "---")) "+"))
+(defn moves [moves piece]
+  (println (str "  |" (s/join "|" (map #(str "  " % "  ") (range 0 8))) "|"))
+  (println (str "--+" (s/join "+" (repeat 8 "-----")) "+"))
   (doseq [row (range 0 8)]
-    (print "|")
+    (print row "|")
     (doseq [col (range 0 8)]
       (print
         (cond
-          (= pos [col row]) (str " " "0" " |")
-          (moves [col row]) (str " " "X" " |")
-          :else " . |")))
-    (print "\n"))
-  (println (str "+" (s/join "+" (repeat 8 "---")) "+")))
+          (= (:pos piece) [col row]) (str " " (display-name piece) " |")
+          (moves [col row]) (str "  " "X" "  |")
+          :else "     |")))
+    (println (str "\n--+" (s/join "+" (repeat 8 "-----")) "+"))))
 
 (defn move-set [rank pos & [color]]
-  (moves (rules/move-set {:rank rank :pos pos :color (or color :white)}) pos))
+  (let [piece {:rank rank :pos pos :color (or color :white)}]
+    (moves (rules/move-set piece) piece)))
